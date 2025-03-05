4a754f9c761905ada81a49576fa7bad839732827
luska777 <118298294+luska777@users.noreply.github.com>
2025-03-05 22:52:56 +0000

    Desafio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,20 +2,25 @@
 
     int main(){
 
-        int numero = 1, i = 1;
+        int numero = 1, i = 1, j, k;
 
         do {printf("Movimento da Rainha: \n Esquerda \n");
             i++;} 
         while (i <= 8);
 
         for (int i = 0; i < 5; i++)
-        {printf("Movimento da peça: \n Cima, Direita \n");}
+        {printf("Movimento do Bispo: \n Cima, Direita \n");}
         
         while (numero <= 5)
         { printf("Movimento da Torre: \n Direita \n");
          numero++;}
 
-        printf("Novo commit\n");    
+         while (j <= 2) j++;
+         { for (k = 1; k <= 2; k++)
+             printf("Movimento do Cavalo: \n Baixo \n");}
+             
+         printf("Movimento do Cavalo: \n Esquerda \n");
+
 
         return 0;
     }
